82a81d8f28aa0ba1a55923491fe29f7e6643ca95
Luigi-Tho <33259668+Luigi-Tho@users.noreply.github.com>
2026-02-03 22:04:55 -0500

    debugging js tests works now.

--- lua/custom/plugins/extra-debug.lua
+++ lua/custom/plugins/extra-debug.lua
@@ -8,61 +8,47 @@ return {
     'mxsdev/nvim-dap-vscode-js',
     dependencies = { 'mfussenegger/nvim-dap' },
   },
-  config = function()
-    require('mason-nvim-dap').setup {
-      ensure_installed = { 'codelldb', 'delve', 'node-debug2-adapter' },
-    }
-    require('dap-vscode-js').setup {
-      adapters = { 'pwa-node', 'codelldb' },
-    }
-    local dap = require 'dap'
-    dap.adapters.codelldb = {
-      type = 'executable',
-      command = 'codelldb', -- or if not in $PATH: "/absolute/path/to/codelldb"
-
-      -- On windows you may have to uncomment this:
-      -- detached = false,
-    }
-    for _, language in ipairs { 'typescript', 'javascript' } do
-      require('dap').configurations[language] = {
-        {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Launch file',
-          program = '${file}',
-          cwd = '${workspaceFolder}',
-        },
-        {
-          type = 'pwa-node',
-          request = 'attach',
-          name = 'Attach',
-          processId = require('dap.utils').pick_process,
-          cwd = '${workspaceFolder}',
-        },
-        {
-          type = 'pwa-node',
-          request = 'launch',
-          name = 'Debug Jest Tests',
-          -- trace = true, -- include debugger info
-          runtimeExecutable = 'node',
-          runtimeArgs = {
-            './node_modules/jest/bin/jest.js',
-            '--runInBand',
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-jest',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-jest' {
+            jestCommand = 'npm test',
           },
-          rootPath = '${workspaceFolder}',
-          cwd = '${workspaceFolder}',
-          console = 'integratedTerminal',
-          internalConsoleOptions = 'neverOpen',
-        },
-        {
-          type = 'pwa-chrome',
-          request = 'launch',
-          name = 'Start Chrome with "localhost"',
-          url = 'http://localhost:3000',
-          webRoot = '${workspaceFolder}',
-          userDataDir = '${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir',
         },
       }
-    end
-  end,
+    end,
+  },
+  {
+    'David-Kunz/jester',
+    config = function()
+      require('dap-vscode-js').setup {
+        debugger_path = vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug',
+        adapters = { 'pwa-node' },
+      }
+      require('jester').setup {
+        dap = {
+          type = 'pwa-node',
+          console = 'integratedTerminal',
+        },
+      }
+      require('dap').adapters['pwa-node'] = {
+        type = 'server',
+        host = 'localhost',
+        port = '3000',
+        executable = {
+          command = 'node',
+          args = { vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js', '3000' },
+        },
+      }
+    end,
+  },
 }
